46bb152dcf7d4a0be5ab85cbaf4615de7be074d1
wladimiryarosh <vladimir.yaroshenko1992@gmail.com>
2018-08-14 09:29:36 +0300

    add html/css/js/gulp/sass project

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    $('#scroll-top').on('click', function () {
+        $('html,body').animate({scrollTop : 0}, 1000);
+    });
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 300){
+            $('#scroll-top').show()
+        }
+        else
+            $('#scroll-top').hide()
+    });
+    $modal = $('#modal');
+    $('#modal-btn').on('click', function () {
+        $modal.css('display', "block")
+    });
+    $('.close').on('click', function () {
+        $modal.css('display', "none")
+    });
+    $(window).on('click', function (event) {
+        if (event.target === $modal[0]) {
+            $modal.css('display', "none");
+        }
+
+    });
+
+
+
+
+    //js
+
+    // var modal = document.getElementById("modal");
+    // var btn = document.getElementById("modal-btn");
+    // var span = document.getElementsByClassName("close")[0];
+    // btn.onclick = function () {
+    //     modal.style.display = "block";
+    // };
+    // span.onclick = function () {
+    //     modal.style.display = "none";
+    // };
+    // window.onclick = function (event) {
+    //     if (event.target === modal) {
+    //         modal.style.display = "none";
+    // }
+    // };
+});
+
+
+
